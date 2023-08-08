69188e1ff85b9f20b116c1a90dba743b646a9679
Jayesh <jayeshthorat98@gmail.com>
2023-08-08 16:22:39 +0530

    download meme feature added

--- app/src/main/java/com/example/retrofitapiapp/MainActivity.kt
+++ app/src/main/java/com/example/retrofitapiapp/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.retrofitapiapp
 
 import android.app.ProgressDialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentValues
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.drawToBitmap
 import com.bumptech.glide.Glide
 import com.example.retrofitapiapp.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+
 
 //https://meme-api.com/gimme
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
@@ -24,6 +36,11 @@ class MainActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
             getData()
         }
+
+        binding.download.setOnClickListener {
+            mSaveMediaToStorage(bitmap = binding.imageView.drawToBitmap())
+        }
+
     }
 
     private fun getData() {
@@ -39,15 +56,42 @@ class MainActivity : AppCompatActivity() {
             ) {
 
                 binding.Title.text=response.body()?.title
-                Glide.with(this@MainActivity).load(response.body()?.url).into(binding.imageView);
+                Glide.with(this@MainActivity).load(response.body()?.url).into(binding.imageView)
 
                 progressDialog.dismiss()
             }
 
             override fun onFailure(call: Call<ResponseDataClass?>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "${t.localizedMessage}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this@MainActivity, t.localizedMessage, Toast.LENGTH_SHORT).show()
                 progressDialog.dismiss()
             }
-        })
-    }
+    })
 }
+
+    private fun mSaveMediaToStorage(bitmap: Bitmap?) {
+        val filename = "${System.currentTimeMillis()}.jpg"
+        var fos: OutputStream? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            this.contentResolver?.also { resolver ->
+                val contentValues = ContentValues().apply {
+                    put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
+                    put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
+                    put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+                }
+                val imageUri: Uri? = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+                fos = imageUri?.let { resolver.openOutputStream(it) }
+            }
+        } else {
+            val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            val image = File(imagesDir, filename)
+            fos = FileOutputStream(image)
+        }
+        fos?.use {
+            bitmap?.compress(Bitmap.CompressFormat.JPEG, 100, it)
+            Toast.makeText(this , "Saved to Gallery" , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+}
+
+
